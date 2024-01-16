98d2852b599442562b98e23d1ae2b83f1c2171be
Pieter Dolmans <p.c.m.dolmans@students.uu.nl>
2024-01-16 15:14:16 +0100

    Part 3 (relative yearly precipitation) + plot

--- analysing-the-weather.py
+++ analysing-the-weather.py
@@ -1,47 +1,58 @@
 import json
 from csv import DictReader
 
-#Puts the contents of the json file into list 'precipitation'
+#Puts contents of json file into list 'precipitation'
 with open('precipitation.json') as file:
     precipitation = json.load(file)
 
-#Creates a list of dictionaries of the measurements at the Seattle station
-precipitation_seattle = list()
-for measurement in precipitation:
-    if measurement['station'] == 'GHCND:US1WAKG0038':
-        precipitation_seattle.append(measurement)
+#Makes list of dictionaries describing the stations
+with open('stations.csv') as file:
+    items = list(DictReader(file))
 
-#Makes dictionary 'results' containing list 'total_monthly_precipitation' with 12 elements (all 0)
+#Makes dictionary 'results'
 results = dict()
-results['total_monthly_precipitation'] = [0,0,0,0,0,0,0,0,0,0,0,0]
-current_month = 0
-#Loops for every month, counts total precipitation and adds it to the list
-for month in results['total_monthly_precipitation']:
-    current_month += 1
-    current_month_index = current_month -1
-    #Fixes month count so it matches with the date entries
-    if current_month <= 9:
-        current_month_str = '0' + str(current_month)
-    else:
-        current_month_str = str(current_month)
-    #calculates total precipitation and adds it to right month
-    for measurement in precipitation_seattle:
-        if measurement['date'].startswith(f'2010-{current_month_str}'):
-            results['total_monthly_precipitation'][current_month_index] += measurement['value']
 
-#Calculates total yearly precipitation
-total_yearly_precipitation = 0
-for measurement in precipitation_seattle:
-    total_yearly_precipitation += measurement['value']
+#Adds all results per location to 'results'
+location_index = -1
+for location in items:
+    location_index += 1
+    #Makes dictionary containing local information
+    results[f'{items[location_index]['Location']}'] = dict()
+    #Makes list of dictionaries of the measurements at the local station
+    precipitation_local = list()
+    for measurement in precipitation:
+        if measurement['station'] == f'{items[location_index]['Station']}':
+            precipitation_local.append(measurement)
+    #Makes dictionary 'results' containing list 'total_monthly_precipitation' with 12 elements (all 0)
+    results[f'{items[location_index]['Location']}']['total_monthly_precipitation'] = [0,0,0,0,0,0,0,0,0,0,0,0]
+    current_month = 0
+    #Loops for every month, counts total precipitation and adds it to the list
+    for month in results[f'{items[location_index]['Location']}']['total_monthly_precipitation']:
+        current_month += 1
+        current_month_index = current_month -1
+        #Fixes month count so it matches with the date entries
+        if current_month <= 9:
+            current_month_str = '0' + str(current_month)
+        else:
+            current_month_str = str(current_month)
+        #calculates total precipitation and adds it to right month
+        for measurement in precipitation_local:
+            if measurement['date'].startswith(f'2010-{current_month_str}'):
+                results[f'{items[location_index]['Location']}']['total_monthly_precipitation'][current_month_index] += measurement['value']
 
-#Adds list 'relative_monthly_precipitation' with 12 elements (all 0) to dictionary 'results'
-results['relative_monthly_precipitation'] = [0,0,0,0,0,0,0,0,0,0,0,0]
-current_month = -1
-#Loops for every month, calculates relative precipitation and adds it to the list
-for month_relative in results['relative_monthly_precipitation']:
-    current_month += 1
-    results['relative_monthly_precipitation'][current_month] = results['total_monthly_precipitation'][current_month] / total_yearly_precipitation
-    
+    #Calculates total yearly precipitation
+    total_yearly_precipitation = 0
+    for measurement in precipitation_local:
+        total_yearly_precipitation += measurement['value']
+
+    #Adds list 'relative_monthly_precipitation' with 12 elements (all 0) to dictionary 'results'
+    results[f'{items[location_index]['Location']}']['relative_monthly_precipitation'] = [0,0,0,0,0,0,0,0,0,0,0,0]
+    current_month = -1
+    #Loops for every month, calculates relative precipitation and adds it to the list
+    for month_relative in results[f'{items[location_index]['Location']}']['relative_monthly_precipitation']:
+        current_month += 1
+        results[f'{items[location_index]['Location']}']['relative_monthly_precipitation'][current_month] = int(results[f'{items[location_index]['Location']}']['total_monthly_precipitation'][current_month]) / total_yearly_precipitation
+        
 #Dumps results into json file            
 with open('results.json', 'w', encoding='utf-8') as file:
     json.dump(results, file, indent=4)
