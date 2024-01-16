66944f90679cf470f91a0b950a2ef25e49a149f8
Pieter Dolmans <p.c.m.dolmans@students.uu.nl>
2024-01-16 17:21:48 +0100

    Part 3 (added all dictionary entries)

--- analysing-the-weather.py
+++ analysing-the-weather.py
@@ -12,12 +12,24 @@ with open('stations.csv') as file:
 #Makes dictionary 'results'
 results = dict()
 
+#Calculates yearly precipitation
+total_yearly_precipitation = 0
+measurement_index = -1
+for measurement in precipitation:
+    measurement_index += 1
+    total_yearly_precipitation += precipitation[measurement_index]['value']
+
 #Adds all results per location to 'results'
 location_index = -1
 for location in items:
     location_index += 1
     #Makes dictionary containing local information
     results[f'{items[location_index]['Location']}'] = dict()
+
+    #Adds station and state to dictionary
+    results[f'{items[location_index]['Location']}']['station'] = items[location_index]['Station']
+    results[f'{items[location_index]['Location']}']['state'] = items[location_index]['State']
+
     #Makes list of dictionaries of the measurements at the local station
     precipitation_local = list()
     for measurement in precipitation:
@@ -40,10 +52,11 @@ for location in items:
             if measurement['date'].startswith(f'2010-{current_month_str}'):
                 results[f'{items[location_index]['Location']}']['total_monthly_precipitation'][current_month_index] += measurement['value']
 
-    #Calculates total yearly precipitation
-    total_yearly_precipitation = 0
+    #Calculates total yearly precipitation and adds it to the dictionary
+    total_yearly_precipitation_local = 0
     for measurement in precipitation_local:
-        total_yearly_precipitation += measurement['value']
+        total_yearly_precipitation_local += measurement['value']
+    results[f'{items[location_index]['Location']}']['total_yearly_precipitation'] = total_yearly_precipitation_local
 
     #Adds list 'relative_monthly_precipitation' with 12 elements (all 0) to dictionary 'results'
     results[f'{items[location_index]['Location']}']['relative_monthly_precipitation'] = [0,0,0,0,0,0,0,0,0,0,0,0]
@@ -51,7 +64,10 @@ for location in items:
     #Loops for every month, calculates relative precipitation and adds it to the list
     for month_relative in results[f'{items[location_index]['Location']}']['relative_monthly_precipitation']:
         current_month += 1
-        results[f'{items[location_index]['Location']}']['relative_monthly_precipitation'][current_month] = int(results[f'{items[location_index]['Location']}']['total_monthly_precipitation'][current_month]) / total_yearly_precipitation
+        results[f'{items[location_index]['Location']}']['relative_monthly_precipitation'][current_month] = int(results[f'{items[location_index]['Location']}']['total_monthly_precipitation'][current_month]) / total_yearly_precipitation_local
+    
+    #Calculates relative yearly precipitation and adds it to the dictionary
+    results[f'{items[location_index]['Location']}']['relative_yearly_precipitation'] = total_yearly_precipitation_local / total_yearly_precipitation
         
 #Dumps results into json file            
 with open('results.json', 'w', encoding='utf-8') as file:
